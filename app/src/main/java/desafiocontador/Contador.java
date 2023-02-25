89e14a7510cb152688cf43eedd10dc0c30fada6d
Rhaera <rauldantas@usp.br>
2023-02-25 18:54:19 -0300

    Final Test

--- app/src/main/java/desafiocontador/Contador.java
+++ app/src/main/java/desafiocontador/Contador.java
@@ -15,7 +15,11 @@ public class Contador {
             	contar(parametroUm, parametroDois);
             
             } catch  (ParametrosInvalidosException exception) {
+
             	//imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+
+                System.out.println("O segundo parâmetro deve ser maior que o primeiro.");
+
             }
 
         }
